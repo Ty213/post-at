587af820322cc8cedfbdd6612ef0c8748cd1debf
Ty Thompson <tylermoko@gmail.com>
2018-04-04 16:10:52 -0400

    adding post at about of page

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
     }
 
     getLocation() {
-      console.log("get location ran");
       if(sessionStorage.lng && sessionStorage.lat) {
         let location = [];
         location.push(sessionStorage.lng);
@@ -38,7 +37,6 @@ class App extends Component {
     }
 
     getPostats(){
-      console.log("get postats ran");
       fetch(`/api/${sessionStorage.lng},${sessionStorage.lat}`)
         .then(res => res.json())
         .then(postats => this.setState({ postats }))
@@ -67,6 +65,7 @@ class App extends Component {
           path="/postat" 
           render={(props) => <Postat {...props} postats= {this.state.byTime} getPostats={this.getPostats.bind(this)} />} 
         />
+        <SubmitPostat />
         </div>
         </Router>
       )
@@ -101,63 +100,3 @@ function Header() {
     </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       postats: null,
-//       fetching: true
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch('/api/postats')
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error(`status ${response.status}`);
-//         }
-//         return response.json();
-//       })
-//       .then(json => {
-//         this.setState({
-//           postats: json.message,
-//           fetching: false
-//         });
-//       }).catch(e => {
-//         this.setState({
-//           message: `API call failed: ${e}`,
-//           fetching: false
-//         });
-//       })
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <p className="App-intro"></p>
-//         <p className="App-intro">
-//           {this.state.fetching
-//             ? 'Fetching message from API'
-//             : this.state.message}
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
